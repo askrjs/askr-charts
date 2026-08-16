d1d555eb6a0ff6c1db1bf275a1907d7cd59dc980
Jeff Repanich <jrepanich@gmail.com>
2026-08-16 14:38:15 -0400

    release: prepare 0.2.0

--- tests/installed-smoke.js
+++ tests/installed-smoke.js
@@ -184,8 +184,8 @@ void api;
   );
 
   const packedManifest = packEntries[0];
-  assert.equal(packedManifest.version, "0.1.10");
-  assert.match(packedManifest.filename, /askrjs-charts-0\.1\.10\.tgz$/);
+  assert.equal(packedManifest.version, "0.2.0");
+  assert.match(packedManifest.filename, /askrjs-charts-0\.2\.0\.tgz$/);
   assert.equal(packedManifest.size > 0, true);
   assert.equal(packedManifest.unpackedSize > 0, true);
   const packedFiles = new Set(packedManifest.files.map(({ path }) => path));
